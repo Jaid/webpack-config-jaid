bf7639b22cbc2a635b24d2ae044a1f45d34d339f
Jaid <jaid.jsx@gmail.com>
2019-01-17 00:05:42 +0100

    Improved config.stats

--- src/index.js
+++ src/index.js
@@ -87,11 +87,11 @@ export default options => {
     config.stats = {
       all: false,
       modules: true,
-      maxModules: 0,
       errors: true,
       warnings: true,
       moduleTrace: true,
       errorDetails: true,
+      chunks: true,
     }
   }
 
