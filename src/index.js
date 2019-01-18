1338463f730d4c90513d56a227ae507dda35928f
Jaid <jaid.jsx@gmail.com>
2019-01-18 07:35:45 +0100

    Set better default value for isDevelopment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {isObject, isArray} from "lodash"
 export default options => {
   options = {
     packageRoot: String(appRootPath),
-    isDevelopment: true,
+    isDevelopment: process.env.NODE_ENV !== "production",
     extra: null,
     type: "lib",
     ...options,
