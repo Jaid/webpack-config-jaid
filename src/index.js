87140a59b8e50af8b7e5cc58f0eb1b13b8ca6ecb
Jaid <jaid.jsx@gmail.com>
2019-04-06 02:20:34 +0200

    Fixed documentation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import generateWebpackConfig from "./generateWebpackConfig"
  * @property {object} [extra={}] Additional Webpack configuration
  * @property {object} [extraProduction={}] Additional Webpack configuration that only gets applied in development mode
  * @property {object} [extraDevelopment={}] Additional Webpack configuration that only gets applied in production mode
- * @property {"cli"|"nodeLib"|"nodeClass"|"universalLib"|"universalClass"|"webapp"|"nodeScript"} [type=null] The project type which will automatically add some configuration
+ * @property {"cli"|"nodeLib"|"nodeClass"|"universalLib"|"universalClass"|"webapp"|"nodeScript"|"photoshopPlugin"} [type=null] The project type which will automatically add some configuration
  * @property {array} [include=["readme.*","README.*","license.*","LICENSE.*"]] Files (relative to project directory) that get copied to dist directory
  * @property {boolean|object} [publishimo=false] Set to true to include publishimo-webpack-plugin, or set as object to add options for the plugin instance
  * @property {boolean|object} [documentation=false] Set to true to include jsdoc-tsd-webpack-plugin, or set as object to add options for the plugin instance
