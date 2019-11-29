3a50b65a18c85799bf636866b63b7a9d84f4a5f6
Jaid <jaid.jsx@gmail.com>
2019-11-29 10:00:27 +0100

    fix: Fixed default options for type `githubAction`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/types/githubAction/index.js
+++ src/types/githubAction/index.js
@@ -3,12 +3,12 @@ import {configureNode, commonTerserOptions} from "src/configFragments"
 export const defaultOptions = () => ({
   terserOptions: {
     ...commonTerserOptions,
-    toplevel: true,
-    include: false,
-    nodeExternals: false,
-    outDir: "build",
-    licenseFileName: false,
   },
+  toplevel: true,
+  include: false,
+  nodeExternals: false,
+  outDir: "build",
+  licenseFileName: false,
 })
 
 export const webpackConfig = () => configureNode({
