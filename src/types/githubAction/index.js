b88549e9f5b2e9e36e9ab3a42cf232630bb41588
Jaid <jaid.jsx@gmail.com>
2019-11-29 09:37:29 +0100

    feat: Added type `githubAction`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/types/githubAction/index.js
@@ -0,0 +1,18 @@
+import {configureNode, commonTerserOptions} from "src/configFragments"
+
+export const defaultOptions = () => ({
+  terserOptions: {
+    ...commonTerserOptions,
+    toplevel: true,
+    include: false,
+    nodeExternals: false,
+    outDir: "build",
+    licenseFileName: false,
+  },
+})
+
+export const webpackConfig = () => configureNode({
+  resolve: {
+    mainFields: ["main", "module"],
+  },
+})
