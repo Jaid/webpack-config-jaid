cae48333ac6c3e34c9dbc6810448a60fc30f712a
Jaid <jaid.jsx@gmail.com>
2019-12-03 14:43:16 +0100

    Formatted
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/with-package/expect.js
+++ test/with-package/expect.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 exports.default = ({packageOutDir}) => {
   const indexPath = path.join(packageOutDir, "index.js")
   const selfText = fs.readFileSync(indexPath, "utf8")
-  expect(selfText).toMatch('"https://"')
+  expect(selfText).toMatch("\"https://\"")
   expect(selfText).toMatch("require(\"ensure-start\")")
   const self = require(indexPath)
   expect(typeof self.default).toBe("string")
