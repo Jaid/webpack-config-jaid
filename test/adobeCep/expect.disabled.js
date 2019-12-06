f3235e6fb816e51d5df2f9f50da4f5d858fb2361
Jaid <jaid.jsx@gmail.com>
2019-12-07 00:28:32 +0100

    Fixed test for type `adobeCep`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ test/adobeCep/expect.disabled.js
@@ -0,0 +1,10 @@
+import fs from "fs"
+import path from "path"
+
+// Renamed from expect.js to expect.disabled.js
+// because compiling expect.js in an webapp test does crash for some reason
+exports.default = ({packageOutDir}) => {
+  const file = path.join(packageOutDir, "CSXS", "manifest.xml")
+  const xml = fs.readFileSync(file, "utf8")
+  expect(xml).toMatch("\"jaid.adobeCepExtension.extension\"")
+}
