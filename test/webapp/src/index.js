e733a5d0a634728b42b4c3955c95dc3314072a15
Jaid <jaid.jsx@gmail.com>
2019-12-21 01:15:06 +0100

    Updated webapp test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/webapp/src/index.js
+++ test/webapp/src/index.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable you-dont-need-lodash-underscore/trim */
+/* eslint-disable no-undef */
+
 import {trim} from "lodash"
-import css from "./style.scss"
+
 import icon from "./perk.png?responsive"
+import css from "./style.scss"
+
+require("offline-plugin/runtime").install()
 
 window.icon = icon
 
-document.body.innerHTML = `<div/>`
+document.body.innerHTML = "<div/>"
 const img = document.createElement("img")
 img.src = icon.toString()
 document.querySelector("body > div").append(img)
-window.message =  `This is a ${process.browser ? "browser" : "non-browser"} environment`
+window.message = `This is a ${process.browser ? "browser" : "non-browser"} environment`
