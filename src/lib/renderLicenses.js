f587e701ae7cbb73a395b943ae9e8679a1d7d6a7
Jaid <jaid.jsx@gmail.com>
2019-12-24 14:00:51 +0100

    fix: Fixed `thirdPartyLicenses.txt output`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/renderLicenses.js
+++ src/lib/renderLicenses.js
@@ -9,7 +9,7 @@ export default licenses => {
   const formatted = sorted.map(license => {
     const text = license.licenseText?.trim() || "No license defined."
     const versionString = license.packageJson?.version ? ` ${license.packageJson.version}` : ""
-    return `### ${license.name}${versionString}n\n${text}`
+    return `### ${license.name}${versionString}\n\n${text}`
   })
   const joined = formatted.join("\n\n")
   return joined
