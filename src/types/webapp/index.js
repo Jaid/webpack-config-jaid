17a6616dda9877f2fc293fbf2dbbd9de8e06e635
Jaid <jaid.jsx@gmail.com>
2020-03-05 18:18:17 +0100

    Changed non-javascript content color
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/types/webapp/index.js
+++ src/types/webapp/index.js
@@ -138,7 +138,7 @@ export default class extends Html {
    */
   getBodyContent() {
     const text = this.title || this.pkg?.name || this.domain || "Loading"
-    const textColor = "#7777"
+    const textColor = "#8888"
     return `<div><main style="position:fixed;margin:0;padding:0;width:100vw;height:100vh;color:${textColor};display:flex;justify-content:center;align-items:center;font-size:200%;font-family:Ubuntu,sans-serif">
       <span>${text}</span></main></div>`
   }
