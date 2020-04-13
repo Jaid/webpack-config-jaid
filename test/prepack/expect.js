249bc3a749201aaa28c14930f3ace3f67b196a8b
Jaid <jaid.jsx@gmail.com>
2020-04-14 01:05:14 +0200

    Removed `prepack`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/prepack/expect.js
+++
@@ -1,5 +0,0 @@
-const path = require("path")
-
-module.exports.default = ({packageOutDir}) => {
-  const self = path.join(packageOutDir, "index.js")
-}
--- test/prepack/src/index.js
+++
@@ -1,7 +0,0 @@
-// __assumeDataProperty(global, "exports", __abstract())
-
-/* __assumeDataProperty(global, "module", __abstract({
-  exports: __abstract(),
-}))*/
-
-export const basic = () => 100 + Number("23")
