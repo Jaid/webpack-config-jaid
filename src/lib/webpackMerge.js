5824159ad281267bf934ed5222c2f4972c32d60e
Jaid <jaid.jsx@gmail.com>
2020-09-26 20:09:36 +0200

    Simplified lib code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/webpackMerge.js
+++ src/lib/webpackMerge.js
@@ -1,9 +1,6 @@
 import webpackMerge from "webpack-merge"
 
 export default (...webpackConfigs) => {
-  const config1 = webpackConfigs[0]
-  const config2 = webpackConfigs[1]
-  // const mergedSmart = webpackMerge.smart(...webpackConfigs)
   const merged = webpackMerge(...webpackConfigs)
   return merged
 }
