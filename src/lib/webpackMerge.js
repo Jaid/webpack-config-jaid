ec8f59bf116655237422259016e2f378864a9a0c
Jaid <jaid.jsx@gmail.com>
2020-09-26 19:48:39 +0200

    Upgraded `webpack-merge` again to v5
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/webpackMerge.js
+++ src/lib/webpackMerge.js
@@ -1,5 +1,9 @@
 import webpackMerge from "webpack-merge"
 
 export default (...webpackConfigs) => {
-  return webpackMerge.smart(...webpackConfigs)
+  const config1 = webpackConfigs[0]
+  const config2 = webpackConfigs[1]
+  // const mergedSmart = webpackMerge.smart(...webpackConfigs)
+  const merged = webpackMerge(...webpackConfigs)
+  return merged
 }
