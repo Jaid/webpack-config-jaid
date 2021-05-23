7be06b204597d423574a17ad5e8fe9bdc1154e1a
GitHub Action <action@github.com>
2021-05-23 23:56:38 +0000

    autofix: jest.config.js should have md5 46414866323843a90c5a3032140a9f7d

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,28 @@
 module.exports = {
   testEnvironment: "node",
-  coverageDirectory: "dist/jest/coverage",
-  collectCoverageFrom: ["src/**"],
+  coverageDirectory: "<rootDir>/dist/jest/coverage",
+  collectCoverageFrom: [
+    "<rootDir>/dist/package/development/**",
+    "!<rootDir>/node_modules/",
+  ],
+  coverageReporters: [
+    "text-summary",
+    "html",
+  ],
   testPathIgnorePatterns: [
-    "/node_modules/",
-    "/dist/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+  ],
+  modulePathIgnorePatterns: [
+    "<rootDir>/dist",
+    "<rootDir>/build",
   ],
   moduleNameMapper: {
     "^root": "<rootDir>",
     "^src": "<rootDir>/src",
     "^lib": "<rootDir>/src/lib",
   },
+  // This is only false by default if there are multiple test files, so we have to force this to be false
+  // https://jestjs.io/docs/configuration#verbose-boolean
+  verbose: false,
 }
