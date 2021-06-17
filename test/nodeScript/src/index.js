a9b75791706df96528bee1610536a5fcb92f2c1c
Jaid <jaid.jsx@gmail.com>
2021-06-17 15:15:21 +0200

    Fixed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/nodeScript/src/index.js
+++ test/nodeScript/src/index.js
@@ -1,5 +1,5 @@
 const nodeEnv = typeof __webpack_runtime_id__ === "string" ? "development" : "production"
-const file = require("path").join(process.cwd(), "dist", "test", "nodeScript", nodeEnv, "testFile.json")
+const file = require("path").join(process.cwd(), "dist", "test", `nodeScript env=${nodeEnv}`, "testFile.json")
 
 const json = JSON.stringify({
   date: Date.now(),
