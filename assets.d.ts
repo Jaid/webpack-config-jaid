0b36ac62e48082147553b30169649f7fad208f0a
Jaid <jaid.jsx@gmail.com>
2021-06-21 16:23:41 +0200

    feat: Added more TypeScript definitions for loaded assets
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- assets.d.ts
+++ assets.d.ts
@@ -1,4 +1,28 @@
-declare module "\*.hbs" {
-  const content: (templateContext?: object) => string;
-  export default content;
+interface ClassNames {
+  [className: string]: string
+}
+
+declare module "*.txt" {
+  const type: string;
+  export default type;
+}
+
+declare module "*.hbs" {
+  const type: (templateContext?: Object) => string;
+  export default type;
+}
+
+declare module "*.css" {
+  const type: ClassNames
+  export default type
+}
+
+declare module "*.sass" {
+  const type: ClassNames
+  export default type
+}
+
+declare module "*.scss" {
+  const type: ClassNames
+  export default type
 }
