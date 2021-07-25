303e1a5748ca9828ba9bffe5a5cf58835081e279
Jaid <jaid.jsx@gmail.com>
2021-07-25 21:50:34 +0200

    Fixed todo comment
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/types/universalLib/index.js
+++ src/types/universalLib/index.js
@@ -47,7 +47,7 @@ export default class extends WebpackConfigType {
     // Normally config.target shouldn't be hardcoded
     // Webpack can decide this one by itself using browserslist
     // But a missing feature currently disallows mixing web targets and node targets
-    // TODO: Remove or improve this hack later
+    // TODO Remove or improve this hack later
     // See https://github.com/webpack/webpack/issues/11660#issuecomment-841625881
     config.target = "web"
     return config
