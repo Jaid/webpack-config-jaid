0017f77ea4e8948c200671b96a0e7fbd7d915dbc
Jaid <jaid.jsx@gmail.com>
2021-07-25 21:01:04 +0200

    fix: Forced universal output to use Webpack target “web”, see https://github.com/webpack/webpack/issues/11660
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/types/universalLib/index.js
+++ src/types/universalLib/index.js
@@ -44,6 +44,12 @@ export default class extends WebpackConfigType {
     if (pkg?.name) {
       config.output.library = this.getLibraryNameFromPkg(pkg)
     }
+    // Normally config.target shouldn't be hardcoded
+    // Webpack can decide this one by itself using browserslist
+    // But a missing feature currently disallows mixing web targets and node targets
+    // TODO: Remove or improve this hack later
+    // See https://github.com/webpack/webpack/issues/11660#issuecomment-841625881
+    config.target = "web"
     return config
   }
 
