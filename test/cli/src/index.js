f708a83ee22b8819795ccae49e865c0cad475cef
Jaid <jaid.jsx@gmail.com>
2021-09-16 22:49:12 +0200

    Removed dependency winston

--- test/cli/src/index.js
+++ test/cli/src/index.js
@@ -1,11 +1,1 @@
-// import winston from "winston"
-
-// const logger = winston.createLogger({
-//   level: "info",
-//   format: winston.format.simple(),
-//   transports: [new winston.transports.Console],
-// })
-
-// logger.info("ABC")
-
 process.stdout.write("info: ABC")
