d0edefefa56ff5c9507711557fa623b47cd447ac
Jaid <jaid.jsx@gmail.com>
2022-06-15 14:06:13 +0200

    Improved test runner
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@ import "jest-extended"
 
 import path from "node:path"
 import {fileURLToPath, pathToFileURL} from "node:url"
+import {promisify} from "node:util"
 
 import fss from "@absolunet/fss"
 import createDebug from "debug"
 import getFolderSize from "get-folder-size"
 import {isFunction} from "lodash-es"
-import pify from "pify"
 import readFileYaml from "read-file-yaml"
 import sortKeys from "sort-keys"
 import webpackModule from "webpack"
@@ -17,7 +17,7 @@ import readableThousands from "../src/lib/readableThousands.js"
 
 const disabledTests = ["webapp", "adobeCep"]
 
-const webpack = pify(webpackModule)
+const webpack = promisify(webpackModule)
 
 const debug = createDebug("test")
 const dirName = path.dirname(fileURLToPath(import.meta.url))
@@ -27,104 +27,125 @@ const {default: webpackConfigJaid} = await import(pathToFileURL(indexPath))
 
 const sizeChanges = []
 
-function addTest(name, meta) {
-  const testName = `${name} ${Object.entries(meta).map(entry => `${entry[0]}=${entry[1]}`).join(" ")}`
-  it(testName, async () => {
-    const packageRoot = path.join(dirName, name)
-    const configPath = path.join(packageRoot, "webpack.config.js")
-    const jaidConfigPath = path.join(packageRoot, "jaidConfig.js")
-    const expectScriptPath = path.join(packageRoot, "expect.js")
-    const outDir = path.join(dirName, "..", "dist", "test", testName)
-    const oldStats = await readFileYaml.default(path.join(outDir, "benchmark.yml"))
-    const packageOutDir = path.join(outDir, "package")
-    const outputObject = (key, value) => {
-      const sortedObject = sortKeys(value, {deep: true})
-      const yamlFile = path.join(outDir, `${key}.yml`)
-      try {
-        fss.outputYaml(yamlFile, sortedObject)
-      } catch {
-        const file = path.join(outDir, `${key}.json5`)
-        fss.outputJson5(file, sortedObject, {space: 2})
-        const input = fss.readJson5(file)
-        fss.outputYaml(yamlFile, input)
-        fss.remove(file)
-      }
-    }
-    const development = meta.env !== "production"
-    const startTime = Date.now()
-    let webpackConfig
-    if (fss.pathExists(configPath)) {
-      const webpackConfigModule = await import(pathToFileURL(configPath))
-      webpackConfig = webpackConfigModule.default(webpackConfigJaid, packageRoot, packageOutDir, development)
-    } else {
-      let importedJaidConfig = {}
-      if (fss.pathExists(jaidConfigPath)) {
-        const importedJaidConfigModule = await import(pathToFileURL(jaidConfigPath))
-        importedJaidConfig = importedJaidConfigModule.default
-        if (isFunction(importedJaidConfig)) {
-          importedJaidConfig = importedJaidConfig({
-            packageRoot,
-            outDir: packageOutDir,
-            development,
-            meta,
-          })
-        }
-      }
-      const jaidConfig = {
-        packageRoot,
-        development,
-        outDir: packageOutDir,
-        ...importedJaidConfig,
-      }
-      outputObject("jaidConfig", jaidConfig)
-      webpackConfig = webpackConfigJaid(jaidConfig)
-    }
-    outputObject("webpack.config", webpackConfig)
-    let stats
-    if (!meta.hmr) {
-      stats = await webpack(webpackConfig)
-    }
-    const benchmark = {
-      ms: Date.now() - startTime,
+const runTest = async (name, testId, meta) => {
+  const startTime = Date.now()
+  const benchmark = {}
+  debug("Running test: %s", testId)
+  const packageRoot = path.join(dirName, name)
+  const configPath = path.join(packageRoot, "webpack.config.js")
+  const jaidConfigPath = path.join(packageRoot, "jaidConfig.js")
+  const expectScriptPath = path.join(packageRoot, "expect.js")
+  const outDir = path.join(dirName, "..", "dist", "test", testId)
+  const oldStats = await readFileYaml.default(path.join(outDir, "benchmark.yml"))
+  const packageOutDir = path.join(outDir, "package")
+  const outputObject = (key, value) => {
+    if (!value) {
+      value = {}
     }
     try {
-      const packageSize = await getFolderSize(packageOutDir)
-      benchmark.packageBytes = packageSize.size
-      benchmark.codeBytes = fss.stat(path.join(packageOutDir, "index.js")).size
-    } catch {}
-    if (oldStats?.packageBytes) {
-      if (benchmark.packageBytes !== oldStats.packageBytes) {
-        sizeChanges.push({
-          name: testName,
-          before: oldStats,
-          after: benchmark,
+      value = sortKeys(value, {deep: true})
+    } catch (error) {
+      debug("Can't sort object %s", key)
+    }
+    const yamlFile = path.join(outDir, `${key}.yml`)
+    try {
+      fss.outputYaml(yamlFile, value)
+    } catch {
+      const file = path.join(outDir, `${key}.json5`)
+      fss.outputJson5(file, value, {space: 2})
+      const input = fss.readJson5(file)
+      fss.outputYaml(yamlFile, input)
+      fss.remove(file)
+    }
+  }
+  const development = meta.env !== "production"
+  let webpackConfig
+  if (fss.pathExists(configPath)) {
+    debug("Using custom webpack config: %s", configPath)
+    const webpackConfigModule = await import(pathToFileURL(configPath))
+    webpackConfig = webpackConfigModule.default(webpackConfigJaid, packageRoot, packageOutDir, development)
+  } else {
+    debug("Using generated webpack config")
+    let importedJaidConfig = {}
+    if (fss.pathExists(jaidConfigPath)) {
+      debug("Using custom jaidConfig: %s", jaidConfigPath)
+      const importedJaidConfigModule = await import(pathToFileURL(jaidConfigPath))
+      importedJaidConfig = importedJaidConfigModule.default
+      if (isFunction(importedJaidConfig)) {
+        importedJaidConfig = importedJaidConfig({
+          packageRoot,
+          outDir: packageOutDir,
+          development,
+          meta,
         })
       }
     }
-    outputObject("benchmark", benchmark)
-    if (stats) {
-      const statsJson = stats.toJson()
-      if (statsJson.errors.length) {
-        throw statsJson.errors[0]
-      }
-      expect(statsJson.errors).toEqual([])
-      outputObject("stats", statsJson)
+    const jaidConfig = {
+      packageRoot,
+      development,
+      outDir: packageOutDir,
+      ...importedJaidConfig,
     }
-    if (fss.pathExists(expectScriptPath)) {
-      const {default: selfTest} = await import(pathToFileURL(expectScriptPath))
-      await selfTest({
-        name,
-        packageRoot,
-        stats,
-        benchmark,
-        webpackConfig,
-        outDir,
-        packageOutDir,
-        expect,
-        development,
-        meta,
+    outputObject("jaidConfig", jaidConfig)
+    const webpackConfigJaidStart = Date.now()
+    webpackConfig = webpackConfigJaid(jaidConfig)
+    benchmark.webpackConfigJaidTime = Date.now() - webpackConfigJaidStart
+  }
+  outputObject("webpack.config", webpackConfig)
+  let stats
+  const compileStartTime = Date.now()
+  if (!meta.hmr) {
+    stats = await webpack(webpackConfig)
+  }
+  const dateNow = Date.now()
+  benchmark.timeTotal = dateNow - startTime
+  benchmark.timeWebpackCompile = dateNow - compileStartTime
+  debug("Webpack compile time: %d ms", benchmark.timeWebpackCompile)
+  if (fss.pathExists(expectScriptPath)) {
+    debug("Running expect script:", expectScriptPath)
+    const {default: selfTest} = await import(pathToFileURL(expectScriptPath))
+    await selfTest({
+      name,
+      packageRoot,
+      stats,
+      benchmark,
+      webpackConfig,
+      outDir,
+      packageOutDir,
+      expect,
+      development,
+      meta,
+    })
+  }
+  try {
+    const packageSize = await getFolderSize(packageOutDir)
+    benchmark.packageBytes = packageSize.size
+    benchmark.codeBytes = fss.stat(path.join(packageOutDir, "index.js")).size
+  } catch {}
+  if (oldStats?.packageBytes) {
+    if (benchmark.packageBytes !== oldStats.packageBytes) {
+      sizeChanges.push({
+        name: testId,
+        before: oldStats,
+        after: benchmark,
       })
     }
+  }
+  outputObject("benchmark", benchmark)
+  if (stats) {
+    const statsJson = stats.toJson()
+    if (statsJson.errors.length) {
+      throw statsJson.errors[0]
+    }
+    expect(statsJson.errors).toEqual([])
+    outputObject("stats", statsJson)
+  }
+}
+
+function addTest(name, meta) {
+  const testId = `${name} ${Object.entries(meta).map(entry => `${entry[0]}=${entry[1]}`).join(" ")}`
+  it(testId, async () => {
+    await runTest(name, testId, meta)
   }, 1000 * 60 * 5) // 5 minutes
 }
 
