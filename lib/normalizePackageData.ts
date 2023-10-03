3c50daf26111c837dadb0226b9c633d6b5bc3594
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-03 03:27:34 +0200

    Reverted
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- lib/normalizePackageData.ts
+++
@@ -1,8 +0,0 @@
-import type {PackageJson} from 'type-fest'
-
-import normalizePackageData from 'normalize-package-data'
-
-export default (pkg: PackageJson) => {
-  normalizePackageData(pkg)
-  return pkg
-}
